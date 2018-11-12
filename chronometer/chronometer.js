96fc8d7f6be85c4d7ce91d1fb3f274912174cefe
Sandra <sandra93nikolic@gmail.com>
2018-11-12 18:25:14 +0100

    chronometer
    chronometer

---
+++ chronometer/chronometer.js
@@ -0,0 +1,24 @@
+const buttonElement = document.getElementById("button");
+const counterElement = document.getElementById("counter");
+let countSeconds;
+let start = true;
+
+buttonElement.addEventListener("click", () => {
+    if (start) {
+     countSeconds = setInterval( () => {
+     const counter = Number(counterElement.textContent);
+     counterElement.textContent = counter +  1;
+    }, 1000);
+    buttonElement.textContent = "STOP";
+}
+else {
+    clearInterval(countSeconds);
+    buttonElement.textContent = 'START';
+}
+start = !start;
+});
+
+
+
+
+
